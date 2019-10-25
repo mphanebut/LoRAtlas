2e29e9901870f914a5cd65a01acfb0a9176eacfd
mphanebut <phanebut@yahoo.com>
2019-10-25 14:31:38 -0700

    Add files via upload

---
+++ roles/jungle/jungle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jungle',
+  templateUrl: './jungle.component.html',
+  styleUrls: ['./jungle.component.css']
+})
+export class JungleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
